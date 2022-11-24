ecbc9510bd30878c7ee79bedbc0b16857b7d4fbf
sanyogromov@mail.ru <sanyogromov@mail.ru>
2022-11-24 14:42:52 +0300

    Start project app

---
+++ app/src/main/java/com/example/tinderanimal/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tinderanimal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this,
+                MainPage.class)), 2700);
+    }
+}
---
+++ app/src/main/java/com/example/tinderanimal/MainPage.java
@@ -0,0 +1,17 @@
+package com.example.tinderanimal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+    }
+}
