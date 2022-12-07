8602510f669c242866e71fcfde2b71b315949403
sanyogromov@mail.ru <sanyogromov@mail.ru>
2022-12-07 08:26:22 +0300

    up

--- app/src/main/java/com/example/tinderanimal/adapter/AnimalAdapter.java
+++ app/src/main/java/com/example/tinderanimal/adapter/AnimalAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -67,7 +68,7 @@ public class AnimalAdapter extends RecyclerView.Adapter<AnimalAdapter.AnimalView
 
         ImageView animalImg;
         TextView animalTitle, descriptionAnimal;
-        Button buttonClick;
+        ImageButton buttonClick;
 
         public AnimalViewHolder(@NonNull View itemView) {
             super(itemView);
